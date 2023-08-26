0b95e083835b729ef3e20051d52dedff76355562
Daniel <fostiakd@gmail.com>
2023-08-26 17:20:14 +0100

    added firebase hosting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Graph from "../components/Graph";
 import { useState } from "react";
 
 function Home() {
-  const [graph, setGraph] = useState("");
   return (
     <div>
       <h1>Share graphs with your friends</h1>
